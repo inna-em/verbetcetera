e401eafefb04fbe9bec8cec65806b17b0d091a85
inna-em <68508996+inna-em@users.noreply.github.com>
2021-09-12 22:22:33 +0300

    Update cheapest_flights_within_k_stops.py

--- hw14/cheapest_flights_within_k_stops.py
+++ hw14/cheapest_flights_within_k_stops.py
@@ -3,6 +3,9 @@ class Solution:
         graph = [[] for _ in range(n)]
         for flight in flights:
             graph[flight[0]].append((flight[1], flight[2]))
+        distances = [float("inf") for _ in range(n)]
+        current_stops = [float("inf") for _ in range(n)]
+        distances[src], current_stops[src] = 0, 0
         heap = []
         heapq.heappush(heap, (0, src, 0))
         while heap:
@@ -12,6 +15,10 @@ class Solution:
             if steps >= k + 1:
                 continue
             for neighbour, cost in graph[node]:
-                heapq.heappush(heap, (dist + cost, neighbour, steps + 1))
+                if dist + cost < distances[neighbour]:
+                    distances[neighbour] = dist + cost
+                    heapq.heappush(heap, (dist + cost, neighbour, steps + 1))
+                elif steps < current_stops[neighbour]:
+                    heapq.heappush(heap, (dist + cost, neighbour, steps + 1))
         return -1
         
